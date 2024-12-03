7d9848718c3c460fa062f89b1718d0aa6cb5d186
Cadmosss <Cadmillobusiness@gmail.com>
2024-12-03 09:40:59 -0300

    Add files via upload

---
+++ Aula02.py
@@ -0,0 +1,8 @@
+print('era um dia chuvoso, estava nublado, a cidade dava uma sensaçâo de estar alheio há tudo, sendo assim continuei') 
+
+nome_usuario = 'Samuel' #tipo -> str = string
+idade_usuario = 23 # tipo-> int= integer
+altura_usuario = 1.65 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print(' Seja bem-vindo ', nome_usuario , 'sua idade é',idade_usuario, 'anos, voce tem', altura_usuario, 'metros . Voce é maior de idade?', maior_idade)
